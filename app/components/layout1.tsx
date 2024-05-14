46b94aa2f3d3451cb4ec76b6386fef9d838553ec
Koray Erdem KILIÇ <korayerdem89@gmail.com>
2024-05-14 14:28:24 +0300

    first commit

---
+++ app/components/layout1.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+import ListItem from "./list-item";
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+const Layout1 = () => {
+  return (
+    <section className="self-stretch [background:linear-gradient(180deg,_#ff4500,_#ff8c00)] overflow-hidden flex flex-col items-center justify-start py-28 px-16 box-border h-auto max-w-full text-left text-21xl text-tertiary2 font-poppins mq450:gap-[20px] mq800:gap-[40px] mq800:py-[73px] mq800:px-8 mq800:box-border mq750:gap-[40px] mq750:py-[73px] mq750:px-8 mq750:box-border">
+      <div className="self-stretch flex flex-row flex-wrap-reverse items-center justify-start gap-[80px] max-w-full mq450:gap-[20px] mq800:gap-[40px] mq750:gap-[40px]">
+        <div className="flex-1 flex flex-col items-start justify-start gap-[32px] min-w-[320px] max-w-full mq800:gap-[16px] mq800:min-w-full mq750:gap-[16px] mq750:min-w-full">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[24px]">
+            <h1 className="m-0 self-stretch relative text-inherit leading-[120%] font-bold font-inherit mq450:text-5xl mq450:leading-[29px] mq800:text-13xl mq800:leading-[38px] mq1050:text-13xl mq1050:leading-[38px]">
+              From Meme to Reality - Transforming Crypto Together
+            </h1>
+            <div className="self-stretch relative text-lg leading-[150%]">
+              EvolveRWA is a unique crypto project that will evolve from a meme token to DeFi and
+              RWA tokens. With our innovative technology and community-centric approach, we are
+              shaping the future of digital assets.
+            </div>
+          </div>
+          <div className="self-stretch flex flex-row items-start justify-start py-2 px-0 gap-[24px] text-xl mq800:flex-wrap mq750:flex-wrap">
+            <ListItem
+              subheadingOne="Adaptive "
+              text="EvolveRWA seamlessly transitions from meme to DeFi and RWA tokens, offering diverse investment opportunities."
+            />
+            <ListItem
+              subheadingOne="Transparency "
+              text="EvolveRWA prioritizes transparency and community engagement, ensuring active user participation."
+            />
+          </div>
+        </div>
+        <div className="flex-1 mq800:w-full mq800:min-w-[320px] mq750:w-full mq750:min-w-[320px] mq450:w-full mq450:min-w-[320px]">
+          <ReactPlayer
+            width="100%"
+            url="https://www.youtube.com/watch?v=ZSTYNYz7dfM&feature=youtu.be"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Layout1;
---
+++ app/components/navbar.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="sticky top-0 z-50   flex items-center justify-between bg-tertiary2 px-50 py-4 border-b border-tertiary2 text-secondary font-poppins text-xl w-full">
+      <div className="w-44 py-1.5 mq450:pl-4 pl-10 ">
+        <div className="relative h-6 bg-[url('/sitelogo-effects-transparent-1@2x.png')] bg-cover bg-no-repeat">
+          <div className="absolute top-1 left-7 w-36 h-4 z-10" />
+        </div>
+      </div>
+
+      <nav className="mq750:hidden flex items-center gap-8 pt-1.5 pr-10">
+        <Link
+          href="https://app.gitbook.com/invite/KKhZbH1TU6J2wkCLpbOz/gnOuhOmIiXOOne338PuV"
+          className="cursor-pointer hover:text-primary transition-colors duration-200 no-underline  text-secondary "
+        >
+          Docs
+        </Link>
+        <span className="cursor-not-allowed hover:text-gray-200 transition-colors duration-200 no-underline text-gray-200 ">
+          How to Buy
+        </span>
+      </nav>
+
+      <button
+        className="mq750:flex hidden focus:outline-none mr-4"
+        onClick={toggleMenu}
+        aria-label="Toggle Menu"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+
+      {isOpen && (
+        <nav className="absolute top-full left-0 right-0 mt-2 py-2 px-4 bg-tertiary2 shadow-lg md:hidden">
+          <Link
+            href="https://app.gitbook.com/invite/KKhZbH1TU6J2wkCLpbOz/gnOuhOmIiXOOne338PuV"
+            className="block cursor-pointer hover:text-primary transition-colors duration-200  no-underline text-secondary"
+          >
+            Docs
+          </Link>
+          <Link
+            href="https://app.gitbook.com/invite/KKhZbH1TU6J2wkCLpbOz/gnOuhOmIiXOOne338PuV"
+            className="block mt-2 cursor-pointer hover:text-primary transition-colors duration-200 no-underline text-secondary"
+          >
+            How to Buy
+          </Link>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
